68ce545271b9395f5878078569404d7b8916d254
XS <1578760644@qq.com>
2026-05-04 14:22:04 +0800

    初始化仓库

---
+++ assets/Scripts/GameManager.ts
@@ -0,0 +1,135 @@
+import { _decorator, Component, instantiate, Label, Node, Prefab } from 'cc';
+import { PlayerController } from './PlayerController';
+const { ccclass, property } = _decorator;
+
+//通过枚举的方法来保存格子的类型
+//枚举正好第一个值为0，第二个值为1
+enum BlockType {
+    BT_NONE,
+    BT_WHITE
+}
+
+//定义游戏状态
+enum GameState {
+    GS_MENU,
+    GS_PLAYING
+}
+
+
+//通过prefeb生成随机地图，有如下几个条件：
+//1.格子有两种类型可见格子和空格子。后面通过判断player落在哪个格子上，落在空格子上就失败
+//2.因为格子只能跳1步或者2步，所以生成的格子不能连续两个为空，否则会跳不过去
+@ccclass('GameManager')
+export class GameManager extends Component {
+
+    //创建prefab属性，在引擎把box添加到空节点GameManager里面
+    @property(Prefab)
+    public boxPrefab: Prefab = null;
+
+    //设置路的长度
+    @property
+    public roadLength = 100;
+
+    //通过一个空数组保存格子类型
+    private _road: BlockType[] = [];
+
+    //引用PlayerController
+    @property(PlayerController)
+    public PlayerController: PlayerController = null;
+
+    //通过引用node节点来实现UI的出现和隐藏
+    @property(Node)
+    public startMenu: Node = null;
+
+    //收集总步数完成后需要更新到组件
+    @property(Label)
+    public stepLabel: Label = null;
+
+    start() {
+        this.setCurState(GameState.GS_MENU)
+        //通过 .node.on 节点来监听事件,最后的this是当前对象
+        this.PlayerController.node.on('JumpEnd', this.onJumpEnd, this)
+    }
+
+    //设置开始游戏的状态切换
+    setCurState(value: GameState) {
+        if (value === GameState.GS_MENU) {
+            this.PlayerController.reset(); //每次生成路之前先重置player位置
+            this.stepLabel.string = '0'; //把UI步数提示更新为0
+            this.generateRoad(); //生成路
+            //禁用player controller的控制有两种方式1.取消控制绑定 2.用bool状态判断是否可以被控制
+            this.PlayerController.setIsCanControl(false);
+            this.startMenu.active = true; //active方法来判断是否勾选node
+
+        } else if (value === GameState.GS_PLAYING) {
+            this.PlayerController.setIsCanControl(true);
+            this.startMenu.active = false; //active方法来判断是否勾选node
+        }
+    }
+
+    //当开始按钮被点击触发此方法
+    onStartButtonClick() {
+        //触发进入游戏状态
+        this.setCurState(GameState.GS_PLAYING)
+    }
+
+    //收集总步数的方法
+    //参数是value也就是总步数
+    onJumpEnd(value: number) {
+        //通过.string方法来修改label的值,因为值是number 需要用.toString()方法强制类型转换赋值给label
+        this.stepLabel.string = value.toString();
+        
+        this.checkResult(value);
+    }
+
+    //通过此方法来判断player是否走到底，从而游戏结束
+    checkResult(totalStep: number) {
+        //当总长度>=路长度的时候，游戏结束跳转回到菜单界面
+        if (totalStep >= this.roadLength) {
+            this.setCurState(GameState.GS_MENU)
+        } else {
+            //通过索引就可以判断当前的跳动位置是不是等于 BT_NONE的格子，如果等于那么游戏失败,跳回到主菜单
+            if (this._road[totalStep] == BlockType.BT_NONE) {
+                this.setCurState(GameState.GS_MENU)
+            }
+        }
+    }
+
+    //通过for循环生成路的方法，生成的路是挂载在GameManager子节点下的
+    generateRoad() {
+        //通过removeAllChildren方法来消除子节点，因为场景每次加载都需要重新生成路
+        this.node.removeAllChildren();
+        this._road = []; //方法开始要保证_road数组为空
+        //通过.push方法把第一个格子添加为白格子
+        this._road.push(BlockType.BT_WHITE);
+
+        //使用随机数生成0代表空格子 1代表白格子
+        //这里判断生成方块的类型
+        for (let i = 1; i < this.roadLength; i++) {
+            //判断前一个格子是否为空，如果是空的就只能生成一个小白块
+            if (this._road[i - 1] === BlockType.BT_NONE) {
+                this._road.push(BlockType.BT_WHITE)
+            } else {
+                //随机数random方法生成从0-1之间的随机小数，在通过round方法给他四舍五入取整，最后只会得到0和1两个数
+                this._road.push(Math.round(Math.random()))
+            }
+        }
+
+        //判断检测第一个格子是白格子，所以往后面40px开始生成格子
+        for (let j = 1; j < this.roadLength; j++) {
+            if (this._road[j] == BlockType.BT_WHITE) { //这里判断road[1] 刚好是上面enum的 BT_WHITE
+                //通过调用instantiate方法来实例化小白块
+                const box = instantiate(this.boxPrefab);
+                box.setParent(this.node); //把prefab绑定在了GameManager下方
+                box.setPosition(j * 40, 0, 0) //初始位置0是小白块，从40px后面开始生成
+            }
+        }
+
+    }
+
+    update(deltaTime: number) {
+
+    }
+}
+
+
---
+++ assets/Scripts/PlayerController.ts
@@ -0,0 +1,140 @@
+import { _decorator, animation, Animation, Component, EventMouse, Input, input, Node, Vec3 } from 'cc';
+const { ccclass, property } = _decorator;
+
+//通过鼠标左键和右键来判断方块移动一步还是两步。因为方块大小是40px，所以一步就是移动40px，两步为80px
+//获取鼠标按键 0左键 1中键 2右键 event.getButton() === 0
+@ccclass('PlayerController')
+export class PlayerController extends Component {
+
+    //标志位，确定是否开始跳跃
+    private _startJump = false;
+    //跳跃时间
+    private _junpTime = 0.2;
+    //跳跃计时器，当前已经花了多少时间
+    private _curJunpTime = 0;
+    //跳跃速度
+    private _jumpSpeed = 0;
+    //设置目标位置,因为通过_curJunpTime>0.2 是通过时间来控制
+    //不能保证player移动的时候一定是40px，因为帧数不稳定，虽然已经算出了跳跃速度，但是不能保证每次都是这么多时间
+    private _targetPos = new Vec3();
+    //当前位置
+    private _curPos = new Vec3();
+
+    //获取动画组件
+    @property(Animation)
+    public bodyAnim: Animation = null;
+
+    //通过_curTotalStep来定义player跳动的步数
+    private _curTotalStep = 0;
+
+
+    start() {
+        // input.on(Input.EventType.MOUSE_DOWN, this.onMouseDown, this);
+    }
+
+    //要在GameManager里面调用次方法来判断player是否可以被控制
+    public setIsCanControl(value: boolean) {
+        if(value){
+            input.on(Input.EventType.MOUSE_DOWN, this.onMouseDown, this);
+        }else{
+            input.off(Input.EventType.MOUSE_DOWN, this.onMouseDown, this);
+        }
+    }
+
+    onMouseDown(event: EventMouse) {
+        if (event.getButton() == 0) {
+            this.JumpByStep(1);
+        } else if (event.getButton() == 2) {
+            this.JumpByStep(2);
+        }
+    }
+
+    //跳跃步数的方法
+    JumpByStep(step: number) {
+        //#region
+        //直接让player像塑移动40px实现跳跃，没有优化
+        //const curPos = this.node.position;
+        //this.node.setPosition(curPos.x + 40 * step, curPos.y, curPos.z);
+        //#endregion
+
+        //如果在跳跃就不执行后面代码
+        if (this._startJump) return;
+
+        //动画播放,动画前置加载。把动画前置后，最开始_jumptime的默认值变得可有可无了
+        // AI总结
+        // 抽搐原因：是因为在 update计算位置前，访问 duration导致动画系统提前把第 0 帧的位置刷到了节点上，造成了瞬移。
+        // 最佳实践：强烈建议你保持现在的写法（动画前置）。让逻辑速度去适配动画时长，这样以后策划改动画时长（比如改成 0.3 秒），你不需要动一行代码，游戏逻辑会自动适配，这是非常高级且专业的代码结构。
+        this.JumpAnimState(step);
+
+        //定义距离
+        const moveLength = step * 40;
+        this._startJump = true;
+        this._curJunpTime = 0;
+        //#region
+        //利用 dt 进行插值：_jumpSpeed是你计算出的“每秒应该移动的距离”。
+        //公式：这一帧的移动距离 = 速度 × 这一帧的时间(dt)
+        //#endregion
+        //速度由总共需要跳跃的 时间/距离 得出
+        this._jumpSpeed = moveLength / this._junpTime;
+
+        //先获取当前位置 这种写法也可以this.node.getPosition(this._curPos)    
+        this._curPos = this.node.position;
+        //目标位置，通过add方法，让Vec3向量相加add(a,b,c) b+c 赋值给a
+        Vec3.add(this._targetPos, this._curPos, new Vec3(moveLength, 0, 0))
+        //上面的另一种写法
+        //this._targetPos = new Vec3(this._curPos.x + step * 40, this._curPos.y, this._curPos.z);
+
+        //通过这个来保存总的步数，或者说当前步数，跳一步+1跳两步+2
+        this._curTotalStep+=step;
+
+    }
+
+    //让动画的时间与跳跃的时间保持一致,取得动画时间并设置给jumptime
+    JumpAnimState(step: number) {
+
+        //通过三元运算符来判断step是否等于1
+        const animName = step == 1 ? 'JumpOneStep' : 'JumpTwoStep'
+        const animState = this.bodyAnim.getState(animName) //通过getState方法选择要传递动画的名字
+        //通过duration方法把动画时间设置给jumptime
+        this._junpTime = animState.duration;
+        this.bodyAnim.play(animName); //.play方法播放动画
+    }
+
+
+    //优化跳跃流程,让运动过程更平滑
+    //屏幕是60帧，现实中时间的1秒，dt是从一帧到下一帧所用的时间，一般是0.0x秒
+    //设置了_JunpTime为0.2，也就是让他跳一下的时候花费0.2秒 _curJunpTime大于0.2秒后就停止跳跃
+    //那么这当中是经过了多少dt也不知道？
+    protected update(dt: number): void {
+        //_startJump为true开始跳跃
+        if (this._startJump) {
+            //让跳跃的时间增加
+            this._curJunpTime += dt;
+            //_curJunpTime大于0.2秒后就停止跳跃
+            if (this._curJunpTime > this._junpTime) {
+                this._startJump = false;
+                this.node.setPosition(this._targetPos);
+
+                //在每一步跳动结束后更新UI显示上面以跳动的步数
+                //通过emit方法来监听发送自定义JumpEnd事件,把总步数传过去
+                this.node.emit("JumpEnd",this._curTotalStep);
+            } else {
+                const curPos = this.node.position;
+                this.node.setPosition(curPos.x + this._jumpSpeed * dt, curPos.y, curPos.z);
+            }
+        }
+    }
+
+    protected onDestroy(): void {
+        input.off(Input.EventType.MOUSE_DOWN, this.onMouseDown, this);
+
+    }
+
+    //通过此方法让游戏失败/结束后player位置重新归零,并更新步数
+    public reset(){
+        this.node.setPosition(0,0,0);
+        this._curTotalStep = 0;  //这里是为了清0总步数，让程序从新从0开始判断
+    }
+}
+
+
